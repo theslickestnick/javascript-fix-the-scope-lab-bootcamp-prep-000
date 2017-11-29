e31a956259c26ee3243f7eeea788edbd3791cd85
Nicholas Echevarria <nick.echev@gmail.com>
2017-11-29 18:47:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,19 +5,13 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
   animal = 'cat'
   return animal
 }
 
 function add2(n) {
-  n = Math.floor(Math.random() * 1000)
-  return n + 2
-
-  // Feel free to move things around!
   const two = 2
+  return n + two
 }
 
 var funkyFunction = function() {

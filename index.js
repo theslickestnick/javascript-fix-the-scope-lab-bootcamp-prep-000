93fd0dd49c1799bf04c8c73bce6deed28e7f71d0
Nicholas Echevarria <nick.echev@gmail.com>
2017-11-29 18:51:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return FUNKY!
+  return function() {
+    return "FUNKY!"
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
